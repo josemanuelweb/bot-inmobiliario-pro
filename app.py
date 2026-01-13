f030b82a230800166af2274d8fc435965d1d46a5
josemanuelweb <manuellchenko@gmail.com>
2026-01-12 23:11:19 -0300

    Fix visual: JavaScript activado

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, send_from_directory
 import os
 
-# Configuramos Flask para que busque el index en la raíz '.' 
-# en lugar de la carpeta 'templates'
 app = Flask(__name__, template_folder='.', static_folder='.')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Ruta para que la web pueda leer el JSON y el Excel
+# Esta ruta permite que el navegador descargue el JSON y el Excel
 @app.route('/<path:path>')
 def send_file(path):
     return send_from_directory('.', path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Usamos el puerto que pide Render por defecto
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
