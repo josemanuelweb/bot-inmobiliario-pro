ead0e3a0e7cbe551994d54180b2558fe2d004b5c
josemanuelweb <manuellchenko@gmail.com>
2026-01-12 23:56:41 -0300

    Lanzamiento oficial Lógica Digital

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, send_from_directory
 import os
 
+# Configuramos para que busque todo en la carpeta principal
 app = Flask(__name__, template_folder='.', static_folder='.')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Esta ruta permite que el navegador descargue el JSON y el Excel
+# Ruta crucial para que el cliente pueda descargar archivos
 @app.route('/<path:path>')
 def send_file(path):
     return send_from_directory('.', path)
 
 if __name__ == '__main__':
-    # Usamos el puerto que pide Render por defecto
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
